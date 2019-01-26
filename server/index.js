fc124f1d48711f3775295863e107742334bff5b8
Bill Murphy <bill559m@gmail.com>
2019-01-26 12:22:52 -0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+// var items = require('../database-mongo');
+
+const app = express();
+
+// UNCOMMENT FOR REACT
+// app.use(express.static(__dirname + '/../react-client/dist'));
+
+// UNCOMMENT FOR ANGULAR
+// app.use(express.static(__dirname + '/../angular-client'));
+// app.use(express.static(__dirname + '/../node_modules'));
+
+app.get('/items', function (req, res) {
+  items.selectAll(function(err, data) {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log('listening on port 3000!');
+});
+
