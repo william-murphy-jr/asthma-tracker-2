e57c7130e44481ec7a3add5e3660ea4dacff2633
Bill Murphy <bill559m@gmail.com>
2019-01-26 16:13:42 -0800

    list items rendering with bootstrap 4 styling

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ListItem from './ListItem.jsx';
 
-const List = ({ items } = props ) => {
+const List = ({ items } = props) => {
   console.log('items: ', items);
   return (
-    <div>
-      <h4> List Component </h4>
-      There are
-      { items.length }
-      items.
-      { items.map((item, index) => {
+    <div className="card">
+      <div className="card-header">
+        Featured
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <span>  { items.length } </span>
+      <span>items. </span>
+      {items.map((item, index) => {
         const key = index + 100;
         return <ListItem item={item} key={key} />;
       })}
--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ListItem = ({ item } = props) => {
   return (
-    <div>
-      { item.comment }
+    <div className="card-body">
+      <p className="card-text">{ item.comment }</p>
     </div>
   );
 };
